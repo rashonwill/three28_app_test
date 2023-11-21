1466caf5af546fed902d0a05562947082da8e85a
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-11-21 15:37:44 -0600

    Update page.tsx

--- app/(explorer)/page.tsx
+++ app/(explorer)/page.tsx
@@ -71,8 +71,9 @@ export default function Page() {
               )}
             </>
           ))}
-          <button className='px-6 py-0.5 text-xl sm:text-2xl md:text-[22px]' aria-label="email Fari support"><a href="mailto:support@letsfari.com">Support</a></button>
-          <button className='px-6 py-0.5 text-xl sm:text-2xl md:text-[22px]' onClick={logOut} aria-label="logout of Fari">Logout</button>
+          <button className='px-6 py-0.5 text-xl sm:text-2xl md:text-[22px]' aria-label="redirect to community"><a href="/">Community</a></button>
+          <button className='px-6 py-0.5 text-xl sm:text-2xl md:text-[22px]' aria-label="email support"><a href="mailto:support@letsfari.com">Support</a></button>
+          <button className='px-6 py-0.5 text-xl sm:text-2xl md:text-[22px]' onClick={logOut} aria-label="logout">Logout</button>
         </Tabs.List>
         <Tabs.Content value='Browse' className='w-full'>
           <Browse />
