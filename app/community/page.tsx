16d94e3fbe9e9ceedc19d605adf35f6f873c4ff8
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-11-22 15:56:29 -0600

    Update page.tsx

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,1 +1,20 @@
-//test
+'use client';
+import React from "react";
+import { useState } from "react";
+import Button from '@/components/atoms/button';
+import Input from '@/components/atoms/input';
+import Typography from '@/components/atoms/typography';
+import { EyeOff, User2 } from 'lucide-react';
+import Link from 'next/link';
+import _ from "underscore";
+import Head from 'next/head'
+
+
+export default function Community() {
+
+  return (
+      <Typography variant='h2' className='max-lg:text-center text-[#323232] max-lg:mt-44'>
+        Welcome to the Coummunity!
+      </Typography>
+  )
+}
