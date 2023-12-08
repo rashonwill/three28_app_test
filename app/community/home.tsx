fff775bdc9a7ec7d6e9f6fa6e07c4d651dad4573
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-08 15:15:50 -0600

    Update home.tsx

--- app/community/home.tsx
+++ app/community/home.tsx
@@ -26,11 +26,11 @@ return (
                     <div className="flex w-full justify-center items-center">
                     <div className="rounded-2xl overflow-hidden shadow-md max-w-md p-4 dark:bg-[#0D0D0D]">
                     <Typography variant='h3' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>R.E.S.P.E.C.T!</Typography>
-                     <Typography variant='h4' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>We prioritize respect! Be respectful of the platform and it's memmbers, no hate speech, promotion of violence, or any inapproriate content.</Typography>
+                     <Typography variant='h5' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>We prioritize respect! Be respectful of the platform and it's memmbers, no hate speech, promotion of violence, or any inapproriate content.</Typography>
                     </div>
                      <div className="rounded-2xl overflow-hidden shadow-md max-w-md p-4 dark:bg-[#0D0D0D]">
                      <Typography variant='h3' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>No Spam!</Typography>
-                     <Typography variant='h4' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>It's ok to promote, but do not spam the community.</Typography>
+                     <Typography variant='h5' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>It's ok to promote, but do not spam the community.</Typography>
                      </div>         
                     
                     </div>
