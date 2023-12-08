9128a9a3f27c04708f9f39fba12aebb943b398ec
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-08 13:50:55 -0600

    Update home.tsx

--- app/community/home.tsx
+++ app/community/home.tsx
@@ -8,8 +8,8 @@ import Typography from '@/components/atoms/typography';
 export default function Home(){
 
 return (
-          <Typography variant='h3' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>
-        Welcome to the <em>Three28</em>community!
+          <Typography variant='h2' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>
+        Welcome to the Three28 community!
       </Typography>
 )
 
