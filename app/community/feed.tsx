78edbca396a1e749ab074892da315e7c8f96f2e1
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-08 13:26:28 -0600

    Update feed.tsx

--- app/community/feed.tsx
+++ app/community/feed.tsx
@@ -1,1 +1,17 @@
+import React from "react";
+import axios from "axios";
+import _ from "underscore";
+import { useState, useEffect, useCallback } from "react";
+import Typography from '@/components/atoms/typography';
 
+
+export default function Home(){
+
+return (
+      <Typography variant='h3' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>
+        FEEDS!
+      </Typography>
+)
+
+  
+}
