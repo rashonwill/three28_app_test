ae1c4dfaae1d9c35fd14b540e5b4113c862bfa3d
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-08 14:06:26 -0600

    Update home.tsx

--- app/community/home.tsx
+++ app/community/home.tsx
@@ -3,14 +3,28 @@ import axios from "axios";
 import _ from "underscore";
 import { useState, useEffect, useCallback } from "react";
 import Typography from '@/components/atoms/typography';
+import Image from 'next/image';
 
 
 export default function Home(){
 
 return (
-          <Typography variant='h2' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>
-        Welcome to the Three28 community!
-      </Typography>
+          <>
+          <div className="flex rounded-base overflow-hidden shadow-card w-full h-1/4">
+                    
+            <div className="flex-1">
+            <Typography variant='h2' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>Welcome to the Three28 community!</Typography>
+          </div>
+            
+            <Image
+        src={'/assets/images/logo.svg'}
+        alt='welcome photo'
+        className="w-full h-full"
+      />
+          </div>
+          </>
+
+
 )
 
   
