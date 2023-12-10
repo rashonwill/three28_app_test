d5cc67222de0fde9e2710085b686e9607a482d2d
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-09 18:39:28 -0600

    Update video-with-options.tsx

--- components/organisms/video-with-options.tsx
+++ components/organisms/video-with-options.tsx
@@ -41,7 +41,7 @@ export function VideoWithOptions({
 
   
   return (
-    <div className='relative rounded-2xl dark:shadow-[#171717] shadow-lg max-lg:flex-row w-full'>
+    <div className='relative rounded-2xl max-lg:flex-row w-full dark:shadow-[#171717] shadow-lg bg-red-400'>
       <Video
         favorite={favorite!}
         watcher={watcher!}
