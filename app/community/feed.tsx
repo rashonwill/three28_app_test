0046dedc37746cae94270aacf15a1a213ce4460c
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-19 09:52:48 -0600

    Update feed.tsx

--- app/community/feed.tsx
+++ app/community/feed.tsx
@@ -4,14 +4,14 @@ import _ from "underscore";
 import { useState, useEffect, useCallback } from "react";
 import Typography from '@/components/atoms/typography';
 import Button from '@/components/atoms/button';
-import { PencilLine } from 'lucide-react';
+import { PenLine } from 'lucide-react';
 
 export default function Feed(){
 
 return (
       <>
             <div className='flex gap-1.5 -translate-y-1/2 max-lg:hidden'>
-            <Button accent className='flex items-center gap-2'>New Post <PencilLine /></Button>
+            <Button accent className='flex items-center gap-2'>New Post <PenLine /></Button>
             </div>
         <Typography variant='h3' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>
         FEEDS!
