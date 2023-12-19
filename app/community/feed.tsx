d412a107aa4c50f293b642b36ed29a69eb974a83
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-19 09:50:17 -0600

    Update feed.tsx

--- app/community/feed.tsx
+++ app/community/feed.tsx
@@ -10,7 +10,7 @@ export default function Feed(){
 
 return (
       <>
-            <div className="w-full h-[10rem]">
+            <div className='flex gap-1.5 -translate-y-1/2 max-lg:hidden'>
             <Button accent className='flex items-center gap-2'>New Post <PencilLine /></Button>
             </div>
         <Typography variant='h3' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>
