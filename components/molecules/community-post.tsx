6c1b00f0d398a7edf0a737a693069358ee02b93a
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-19 15:40:32 -0600

    Update community-post.tsx

--- components/molecules/community-post.tsx
+++ components/molecules/community-post.tsx
@@ -19,16 +19,19 @@ export default function Post({
   handle,
   user,
   avatar,
+  originator,
 }: {
   src?: string;
   title?: string;
   handle: string; 
   user: string;
   avatar: string;
+  originator?: boolean;	
 }) {
 return (
     <>
-    <div className="flex flex-col justify-center mt-1 w-9/12 dark:bg-[#0D0D0D]">
+	(originator ? (
+        <div className="flex flex-col justify-center mt-1 w-9/12 dark:bg-[#0D0D0D]">
 	<Link href={`/channel?profile=${handle}`} aria-label="visit channel" >
         <div className='flex items-center '>
           <Avatar src={avatar} className='origin-left scale-50' />
@@ -53,6 +56,36 @@ return (
       <Button accent className="flex gap-1.5 rounded-full p-4 dark:bg-[#0D0D0D]">Comment <MessageCircle /></Button>
       </div>
       </div>
+	    
+	) : (
+<div className="flex flex-col justify-center mt-1 w-9/12 dark:bg-[#0D0D0D]">
+	<Link href={`/channel?profile=${handle}`} aria-label="visit channel" >
+        <div className='flex items-center '>
+          <Avatar src={avatar} className='origin-left scale-50' />
+          <Typography variant='h5'>{user}</Typography>
+        </div>
+	</Link>
+      
+      <div className="w-full h-full">
+        <Typography variant="h4">{title}</Typography>
+        <div className="">
+        <Image
+          className='w-full h-9/12'
+          src={src}
+          alt='post image'
+          />
+        </div>
+      
+      </div>
+     <Separator.Root className='w-full h-px my-6 bg-black/40 dark:bg-white/40' />
+      <div className="flex gap-1.5 p-4">
+      <Button accent className="flex gap-1.5 rounded-full p-4 dark:bg-[#0D0D0D]">Like <ThumbsUp /></Button>
+      <Button accent className="flex gap-1.5 rounded-full p-4 dark:bg-[#0D0D0D]">Comment <MessageCircle /></Button>
+      </div>
+      </div>
+	    
+	) )    
+    
     </>
   );
 }
