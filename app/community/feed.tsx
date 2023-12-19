5a73826422ed7b0d60a6f1ec13765419c62a18e6
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-19 09:49:24 -0600

    Update feed.tsx

--- app/community/feed.tsx
+++ app/community/feed.tsx
@@ -3,14 +3,22 @@ import axios from "axios";
 import _ from "underscore";
 import { useState, useEffect, useCallback } from "react";
 import Typography from '@/components/atoms/typography';
-
+import Button from '@/components/atoms/button';
+import { PencilLine } from 'lucide-react';
 
 export default function Feed(){
 
 return (
-      <Typography variant='h3' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>
+      <>
+            <div className="w-full h-[10rem]">
+            <Button accent className='flex items-center gap-2'>New Post <PencilLine /></Button>
+            </div>
+        <Typography variant='h3' className='max-lg:text-center text-[#171717] text-center max-lg:mt-44 mt-4rem dark:text-[#fdfbf9]'>
         FEEDS!
       </Typography>
+      
+      </>
+    
 )
 
   
