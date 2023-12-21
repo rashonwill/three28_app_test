2ac94fa933ed7f88f55e24ada9767b1384666351
rashonwill <75400387+rashonwill@users.noreply.github.com>
2023-12-21 17:21:21 -0600

    Update feed.tsx

--- app/community/feed.tsx
+++ app/community/feed.tsx
@@ -81,13 +81,13 @@ return (
       <>
             <div className='flex justify-center mt-20 gap-1.5 -translate-y-1/2 max-lg:hidden '>
 {/* 		                <Button accent className='px-4 flex items-center gap-2 h-'>New Post <PenLine /></Button> */}
-                <div className=" w-9/12 h-[5rem] rounded-3xl flex flex-col justify-center items-center p-4 dark:bg-[#0D0D0D]">
+                <div className=" w-9/12 h-[7rem] rounded-3xl flex flex-col justify-center items-center p-4 dark:bg-[#0D0D0D]">
 			
                   <div className="h-full w-9/12">
-                  <InputEdit placeholder='New Post' btn='Post' onClick={newComment} onChange={updatePost} value={newpost} />
+                  <InputEdit className="h-[1rem]" placeholder='New Post' btn='Post' onClick={newComment} onChange={updatePost} value={newpost} />
                   </div>
 		    
-		    <Separator.Root className='w-9/12 h-px my-6 bg-black/40 dark:bg-white/40' />
+		    <Separator.Root className='w-9/12 h-px mt-2 bg-black/40 dark:bg-white/40' />
 		<NewPostModal>    
             <div className="flex justify-center gap-1.5 cursor-pointer w-9/12">
             <Typography variant="h5">Photo/Video</Typography><Camera />
