a747b508f333825c04556eac65d1fc7a74998250
rashonwill <75400387+rashonwill@users.noreply.github.com>
2024-02-16 10:39:38 -0600

    Create appsignal.tsx

---
+++ appsignal.tsx
@@ -0,0 +1,6 @@
+// const Appsignal = require("@appsignal/javascript").default; // For CommonJS module
+import Appsignal from "@appsignal/javascript";
+
+export const appsignal = new Appsignal({
+  key: "24cd51af-74da-4094-8b48-e936fd44eb50",
+});
