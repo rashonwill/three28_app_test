b0c4194bd478bf4817b76458ea0ab6202ef8fa65
rashonwill <75400387+rashonwill@users.noreply.github.com>
2024-02-16 22:42:41 -0600

    Update uploads.tsx

--- app/(profile)/channel/uploads.tsx
+++ app/(profile)/channel/uploads.tsx
@@ -9,7 +9,7 @@ import { viewsConversion } from '@/app/conversions/conversion';
 export default function Uploads() {
  const [videos, setVideos] = useState<any[]>([]);
   // const FARI_API = 'https://www.fariapi.com/api';
-  const FARI_API = 'https://fari-prod.herokuapp.com/api';
+  const FARI_API = 'https://three28-test-api.onrender.com/api';
 
  
  const videoPlay = (uuid: any, channelID: any) =>{
