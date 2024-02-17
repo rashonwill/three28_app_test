1b9910f48d67fbe18dca0b0055a29c4a22fdecd0
rashonwill <75400387+rashonwill@users.noreply.github.com>
2024-02-16 22:40:16 -0600

    Update message-modal.tsx

--- app/(profile)/channel/message-modal.tsx
+++ app/(profile)/channel/message-modal.tsx
@@ -17,7 +17,7 @@ export default function MessageModal({
   const [progress, setProgress] = useState({started: false, rate: 0});
   const [message, setMessage] = useState('');
   // const FARI_API = 'https://www.fariapi.com/api';
-  const FARI_API = 'https://fari-prod.herokuapp.com/api';
+  const FARI_API = 'https://three28-test-api.onrender.com/api';
 
     const updateMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewMessage(event.target.value)
