6a4fb728e0824d8b68fbd88f11c9263ed8d5f793
rashonwill <75400387+rashonwill@users.noreply.github.com>
2024-02-16 22:32:01 -0600

    Update page.tsx

--- app/(auth)/reset/page.tsx
+++ app/(auth)/reset/page.tsx
@@ -13,7 +13,7 @@ export default function Reset(){
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
     // const FARI_API = 'https://www.fariapi.com/api';	
-  const FARI_API = 'https://fari-prod.herokuapp.com/api';
+  const FARI_API = 'https://three28-test-api.onrender.com/api';
 
 
   const updateEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
